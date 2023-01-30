2ce0262431cc8a6f9d029b1c04ed3371d48dc55a
Jade Stockings <jade@spaceaccountant.me>
2023-01-30 14:47:29 -0700

    SCSS refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,76 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { RouterLink, RouterView, useRouter } from 'vue-router';
+
+const site_nav_links = [
+  {
+    path: '/',
+    text: 'Home'
+  },
+  {
+    path: '/projects',
+    text: 'Projects'
+  },
+  {
+    path: '/contact',
+    text: 'Contact'
+  }
+];
+
+const current_route_name = computed( () => useRouter().currentRoute.value.name );
 </script>
 
 <template>
   <div id="main-container">
-    <header>
+    <header class="container">
       <div id="portrait">
         <img src="https://pbs.twimg.com/media/ExQ6TijWgAAJPIx?format=png&name=4096x4096" alt="Dorothy Haze" />
         <p><small>Image by <a href="https://twitter.com/_DJSaturn">DJSATURN</a></small></p>
       </div>
-      <div>
+      <div id="description">
         <div id="welcome">
           <h2>Welcome!</h2>
           <p>Hihi! I'm Jade, your average trans girl programmer who likes *nix! I'm working on a number of projects, some public, some private.
              Feel free to check out my interactive projects and eveything else I put on here! If you enjoy my work and want to support me then
-            you can <a href="https://www.buymeacoffee.com/JadeOnIce">buy me a coffee.</a></p>
+             you can <a href="https://www.buymeacoffee.com/JadeOnIce">buy me a coffee.</a></p>
         </div>
         <nav id="content-nav">
-          <ul>
-            <li><RouterLink to="/">Home</RouterLink></li>
-            <li><RouterLink to="/projects">Projects</RouterLink></li>
-            <li><RouterLink to="/contact">Contact</RouterLink></li>
+          <ul v-for="link in site_nav_links">
+            <li><RouterLink v-bind:to='link.path'>{{link.text}}</RouterLink></li>
           </ul>
         </nav>
       </div>
     </header>
     <hr>
-    <div id="content">
-      <h1>{{useRouter().currentRoute.value.name}}</h1>
+    <main id="main-content">
+      <h1>{{current_route_name}}</h1>
       <RouterView />
-    </div>
+    </main>
   </div>
 </template>
 
 <style lang="scss">
-@import '@/assets/style/global.scss';
+@import '@/assets/style/default.scss';
+
+body {
+    background-color: $primary_color;
+    margin: 0 50px;
+}
 
 #main-container {
   color: white;
-  font-family: "IBM VGA",monospace !important;
+  font-family: $font;
   font-size: 20px;
+
+  p {
+    margin: 0;
+    padding: 0;
+  }
 
   h1, h2, h3, h4, h5, h6 {
     color: $secondary_color;
+    margin: 0;
+    padding: 0;
   }
 
   a {
@@ -50,13 +78,14 @@ import { RouterLink, RouterView, useRouter } from 'vue-router';
   }
 
   a:hover, a:active {
-    color: mix($tertiary_color, white, 40%);
+    color: $link_hover_color;
   }
 }
 </style>
 
 <style lang="scss" scoped>
-@import '@/assets/style/global.scss';
+@import '@/assets/style/default.scss';
+@import '@/assets/style/container.scss';
 
 #main-container {
   background-color: rgba(black, 0.3);
@@ -68,10 +97,6 @@ import { RouterLink, RouterView, useRouter } from 'vue-router';
   padding: 30px;
 
   header {
-    column-gap: 30px;
-    display: grid;
-    grid-template-columns: auto auto;
-    justify-content: start;
     margin-bottom: 30px;
 
     #portrait {
@@ -90,35 +115,24 @@ import { RouterLink, RouterView, useRouter } from 'vue-router';
       }
     }
 
-    #welcome {
-      text-align: center;
-    }
-
-    #content-nav {
-      font-size: 24px;
-      padding: 20px 0;
+    #description {
       text-align: center;
 
-      ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
+      #content-nav {
+        font-size: 24px;
+        margin-top: 30px;
+
+        ul {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+        }
       }
     }
   }
 
-  @media screen and (max-width: 800px) {
-    header {
-      display: block;
-
-      #portrait {
-        margin-bottom: 30px;
-      }
-    }
-  }
-
-  #content {
-    padding: 20px 0;
+  #main-content {
+    margin-top: 30px;
     text-align: center;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/style/global.scss'
-
 const app = createApp(App)
 
 app.use(router)
