c7170e854463a7d9b3656e45d3f7274114c2cfc7
Jade Stockings <jade@spaceaccountant.me>
2023-01-30 15:44:49 -0700

    Created Copyable component

---
+++ src/components/Copyable.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+  content: string
+}>();
+
+function copy() {
+  navigator.clipboard.writeText( props.content );
+}
+</script>
+
+<template>
+  <span class="copyable" v-on:click="copy()"><slot></slot></span>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/style/default.scss';
+
+.copyable {
+  color: $secondary_color;
+  text-decoration: underline;
+}
+
+.copyable:hover, .copyable:active {
+  color: $link_hover_color;
+  cursor: pointer;
+}
+</style>
